428152f90c114bff1322eff218fa5f62f81cad5b
ajbc <ajbchaney@gmail.com>
2015-03-29 00:37:25 +0000

    added doc model and removed .pyc files

---
+++ timesig/myapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from timesig.myapp.models import Doc
+
+admin.site.register(Doc)
--- timesig/myapp/models.py
+++ timesig/myapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Doc(models.Model):
+    title = models.CharField(max_length=80)
+    author = models.CharField(max_length=80)
+    filename = models.CharField(max_length=80)
