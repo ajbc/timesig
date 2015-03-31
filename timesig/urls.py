5e7395052ba3537b358fcdd01dbd9062ad6933c6
ajbc <ajbchaney@gmail.com>
2015-03-31 17:02:56 +0000

    added CSS styling file and fixed menu link to profile page

--- timesig/urls.py
+++ timesig/urls.py
@@ -4,6 +4,9 @@ from django.contrib.auth.views import login, logout
 from django.contrib import admin
 admin.autodiscover()
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^blog/', include('blog.urls')),
@@ -13,4 +16,4 @@ urlpatterns = patterns('',
     (r'^accounts/logout/$', logout),
     url(r'^accounts/profile/$', 'timesig.myapp.views.profile', name='profile'),
     url(r'^accounts/register/$', 'timesig.myapp.views.register', name='register'),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
